f026b01324a8b1e7051cce35360bc59a3db05870
fabysshk <fabysshk@gmail.com>
2020-09-01 17:09:55 +0300

    base spa routing

--- src/App.svelte
+++ src/App.svelte
@@ -1,74 +1,19 @@
 <script>
-	import Task from "./Task.svelte";
-	import AddTask from "./AddTask.svelte";
-
-	let tasks = [
-		{taskText: "Homework :)", done: false},
-		{taskText: "build an app", done: true,},
-		{taskText: "world domination", done: false,}
-	];
-	const deleteTask = e => tasks = [...tasks].filter((_, i) => i !== e.detail);
-	const findTask = index => tasks[index];
-	const doneTask = e => findTask(e.detail).done = !findTask(e.detail).done;
-	const createTask = taskText => ({taskText, done: false});
-	const addTask = e => tasks = [createTask(e.detail),...tasks];
-	const clearTasks = () => tasks = tasks.filter(task => !task.done);
+  import Router from 'svelte-spa-router';
+  import Header from './components/Header.svelte';
+  import Footer from './components/Footer.svelte';
+  import Home from './pages/Home.svelte';
+  import Profile from './pages/Profile.svelte';
+  import Login from './pages/Login.svelte';
+  import Register from './pages/Register.svelte';
+  import NotFound from './pages/NotFound.svelte';
+  const routes = new Map();
+  routes.set('/', Home);
+  routes.set('/profile', Profile);
+  routes.set('/login', Login);
+  routes.set('/register', Register);
+  routes.set('*', NotFound);
 </script>
-
-<main>
-
-	<h1>Svelte ToDo</h1>
-
-	<AddTask on:add={addTask}
-		     on:clear={clearTasks}
-	/>
-
-	<br/>
-
-	<div class="tasks">
-		{#each tasks as task, index}
-			<Task   {...task}
-					index={index}
-					on:deleted={deleteTask}
-					on:done={doneTask}
-			/>
-		{/each}
-	</div>
-
-</main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	.tasks {
-		min-height: 500px;
-		margin: 0 auto;
-		padding: 10px;
-		text-align: left;
-		width: 400px;
-		/* border: 1px solid red; */
-	}
-
-	button {
-		background-color: #fff;
-		color: red;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+<header><Header/></header>
+<main><Router {routes}/></main>
+<footer><Footer/></footer>
